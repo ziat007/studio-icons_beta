93a5bbdac13ea8577559d9e209f04fad78b2ad75
ziat007 <samjohnking@gmail.com>
2024-07-06 14:09:25 +0200

    Update build-script.js

--- build-script.js
+++ build-script.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { minify } = require('terser');
 
 // Read the template files
 const templateStart = fs.readFileSync('src/template-start.js', 'utf8');
@@ -20,11 +21,44 @@ files.forEach(file => {
 });
 
 // Generate the final content
-const finalContent = `${templateStart}
-var iconTemplates = ${JSON.stringify(iconTemplates, null, 2)};
+const unminifiedContent = `${templateStart}
+var iconTemplates = ${JSON.stringify(iconTemplates)};
 ${templateEnd}`;
 
-// Write the final content to the output file
-fs.writeFileSync('src/studio-icons_beta.js', finalContent);
+// Minify the content
+async function buildAndMinify() {
+  try {
+    const minifiedResult = await minify(unminifiedContent, {
+      mangle: true,
+      compress: {
+        dead_code: true,
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        booleans: true,
+        loops: true,
+        unused: true,
+        hoist_funs: true,
+        keep_fargs: false,
+        hoist_vars: true,
+        if_return: true,
+        join_vars: true,
+        cascade: true,
+        side_effects: true,
+        warnings: false
+      }
+    });
 
-console.log('studio-icons_beta.js has been generated successfully.');
+    // Write the minified content to the output file
+    fs.writeFileSync('src/studio-icons_beta.min.js', minifiedResult.code);
+    console.log('studio-icons_beta.min.js has been generated successfully.');
+
+    // Optionally, you can also save the unminified version
+    fs.writeFileSync('src/studio-icons_beta.js', unminifiedContent);
+    console.log('studio-icons_beta.js (unminified) has been generated successfully.');
+  } catch (error) {
+    console.error('Minification failed:', error);
+  }
+}
+
+buildAndMinify();
